f8f8786f1f9362e592b6b014800024a3d64d72cd
Francisco Zanon <panchizanon@gmail.com>
2024-07-03 21:05:05 -0300

    entrega 2 v.2

--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { FaShoppingCart } from "react-icons/fa";
-import { Box } from "@chakra-ui/react";
+import { Box, Flex } from '@chakra-ui/react';
 const CartWidget = () => {
   return (
-    <Box mr={4}>
-      <FaShoppingCart size={24}/>
-    </Box>
+    <Flex align="center" mr={4}>
+      <FaShoppingCart size={24} />
+      <Box ml={2}>
+        <p>3</p>
+      </Box>
+    </Flex>
   );
 };
 
